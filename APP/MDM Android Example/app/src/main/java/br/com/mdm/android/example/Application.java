29fc68fff1952c3dcf168b16b9379234f0235048
Bruno Reis <bruno.reis@hands.com.br>
2020-05-25 01:51:24 -0300

    MDM 3.6.1

--- APP/MDM Android Example/app/src/main/java/br/com/mdm/android/example/Application.java
+++ APP/MDM Android Example/app/src/main/java/br/com/mdm/android/example/Application.java
@@ -2,6 +2,7 @@ package br.com.mdm.android.example;
 
 import br.com.hands.mdm.libs.android.bundle.MDMBundle;
 import br.com.hands.mdm.libs.android.core.MDMCore;
+import br.com.hands.mdm.libs.android.core.OnStartListener;
 
 public class Application extends android.app.Application {
     @Override
@@ -11,18 +12,29 @@ public class Application extends android.app.Application {
         // Debug Mode
         MDMCore.setDebugMode(true);
 
+        String appId = "SEU_APP_ID";
 
         // Start do Bundle com todos os módulos
-        MDMBundle.start(getApplicationContext());
+        MDMCore.start(getApplicationContext(), appId, new OnStartListener() {
+            @Override
+            public void onStart() {
+                MDMBundle.start(getApplicationContext());
+            }
+        });
 
         //
         // OU
         //
 
         // Start de cada módulo separadamente
-        // MDMGeoBehavior.start(getApplicationContext());
-        // MDMAppBehavior.start(getApplicationContext());
-        // MDMNotification.start(getApplicationContext());
+        /*MDMCore.start(getApplicationContext(), appId, new OnStartListener() {
+            @Override
+            public void onStart() {
+                MDMGeoBehavior.start(getApplicationContext());
+                MDMAppBehavior.start(getApplicationContext());
+                MDMNotification.start(getApplicationContext());
+            }
+        });*/
     }
     @Override
     public void onTerminate() {
