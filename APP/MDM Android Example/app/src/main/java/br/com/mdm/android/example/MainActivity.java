886435b364c28bce5118221a38ed79c30f95f513
Bruno Reis <bruno.reis@hands.com.br>
2021-07-19 16:39:06 -0300

    MDM 3.7.12

--- APP/MDM Android Example/app/src/main/java/br/com/mdm/android/example/MainActivity.java
+++ APP/MDM Android Example/app/src/main/java/br/com/mdm/android/example/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.mdm.android.example;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
 import br.com.hands.mdm.libs.android.appbehavior.MDMAppBehavior;
+import br.com.hands.mdm.libs.android.core.MDMCore;
 import br.com.hands.mdm.libs.android.geobehavior.MDMGeoBehavior;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
@@ -20,13 +22,23 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Pedir permissão de Geolocalização ao usuário e inicia o módulo de GeoBehavior
-        requestGeoTrackingPermissions();
-
-        // Inicia o módulo de AppBehavior
-        if (!MDMAppBehavior.hasPermissionToRun(getApplicationContext())) {
-            MDMAppBehavior.askPermission(this);
-        }
+        final Activity activity = this;
+        MDMCore.askPermissionFullDialog(this, new MDMCore.PermissionListener() {
+            @Override
+            public void onPermissionResponse(boolean response) {
+                if (response && !MDMAppBehavior.hasAskedPermission(getApplicationContext())) {
+                    MDMAppBehavior.setPermission(getApplicationContext(), true);
+                    MDMAppBehavior.start(activity.getApplicationContext());
+                } else {
+                    if (!MDMAppBehavior.hasPermissionToRun(getApplicationContext()) &&
+                            !MDMAppBehavior.hasAskedPermission(getApplicationContext())) {
+                        MDMAppBehavior.askPermission(activity);
+                    }
+                }
+                // Pedir permissão de Geolocalização ao usuário e inicia o módulo de GeoBehavior
+                requestGeoTrackingPermissions();
+            }
+        });
     }
 
     // Método exemplo para pedir permissão de geolocalização
@@ -44,30 +56,30 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
         // Verifica permissão de geolocalização
         if (ActivityCompat.checkSelfPermission(getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                /*android.Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getApplicationContext(),*/
                 android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getApplicationContext(),
-                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getApplicationContext(),
                 android.Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
             // Requisitar permissão de geolocalização
             ActivityCompat.requestPermissions(this, new String[]{
                     android.Manifest.permission.ACCESS_COARSE_LOCATION,
                     android.Manifest.permission.ACCESS_FINE_LOCATION,
-                    android.Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-                    android.Manifest.permission.ACTIVITY_RECOGNITION
-            }, 200);
+                    //android.Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                    android.Manifest.permission.ACTIVITY_RECOGNITION}, 1);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (200 == requestCode && grantResults.length > 0) {
+        if (requestCode == 1 && grantResults.length > 0) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //Permissão concedida, inicializar módulo GeoBehavior
                 MDMGeoBehavior.start(getApplicationContext());
+                ActivityCompat.requestPermissions(this, new String[]{
+                        android.Manifest.permission.ACCESS_BACKGROUND_LOCATION}, 1);
             } else {
                 // Permissão não concedida
             }
